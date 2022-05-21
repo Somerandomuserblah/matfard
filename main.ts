20aa3b8050b5085487169d26eb5d413fa6a3c66d
Mudkip909 <76056631+Mudkip909@users.noreply.github.com>
2022-05-21 11:44:37 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,35 @@
-
+let out1: number[] = []
+let out2: number[] = []
+let out3: number[] = []
+let det = 0
+let aRow4: number[] = []
+let aRow3: number[] = []
+let aRow2: number[] = []
+let aRow1: number[] = []
 let mRow1 = [
-1,
-5,
-5,
-2
+0,
+0,
+0,
+0
 ]
 let mRow2 = [
-5,
-6,
-2,
-8
+0,
+0,
+0,
+0
 ]
 let mRow3 = [
-2,
-6,
-9,
-5
+0,
+0,
+0,
+0
 ]
 let mRow4 = [
-4,
-5,
-1,
-2
+0,
+0,
+0,
+0
 ]
-let aRow1: number[] = []
-let aRow2: number[] = []
-let aRow3: number[] = []
-let aRow4: number[] = []
-let det = 0;
 namespace matStuff {
     //%block
     export function matrixInverseFixed(in1: Array<int32>, in2: Array<int32>, in3: Array<int32>, in4: Array<int32>): Array<Array<int32>> {
@@ -63,4 +65,34 @@ namespace matStuff {
         ];
 
     }
+
+    //%block
+    export function multMat(inA1: Array<int32>, inA2: Array<int32>, inA3: Array<int32>, inB1: Array<int32>, inB2: Array<int32>, inB3: Array<int32>): Array<Array<int32>> {
+        /*
+        let inA3: number[] = []
+        let inA2: number[] = []
+        let inB3: number[] = []
+        let inB2: number[] = []
+        let inB1: number[] = []
+        let inA1: number[] = []
+        */
+        out1 = [0, 0, 0]
+        out2 = [0, 0, 0]
+        out3 = [0, 0, 0]
+        out1[0] = inA1[0] * inB1[0] + inA1[1] * inB2[0] + inA1[2] * inB3[0]
+        out1[1] = inA1[0] * inB1[1] + inA1[1] * inB2[1] + inA1[2] * inB3[1]
+        out1[2] = inA1[0] * inB1[2] + inA1[1] * inB2[2] + inA1[2] * inB3[2]
+        out2[0] = inA2[0] * inB1[0] + inA2[1] * inB2[0] + inA2[2] * inB3[0]
+        out2[1] = inA2[0] * inB1[1] + inA2[1] * inB2[1] + inA2[2] * inB3[1]
+        out2[2] = inA2[0] * inB1[2] + inA2[1] * inB2[2] + inA2[2] * inB3[2]
+        out3[0] = inA3[0] * inB1[0] + inA3[1] * inB2[0] + inA3[2] * inB3[0]
+        out3[1] = inA3[0] * inB1[1] + inA3[1] * inB2[1] + inA3[2] * inB3[1]
+        out3[2] = inA3[0] * inB1[2] + inA3[1] * inB2[2] + inA3[2] * inB3[2]
+        return [
+            out1,
+            out2,
+            out3,
+        ];
+    }
+
 }
